c012645c8e19035621d65d5ebaf3cb4032cfc8ed
suparnagrawal <suparnagrawal8@gmail.com>
2025-05-14 01:50:22 +0530

    final converter

--- lib/converter_page.dart
+++ lib/converter_page.dart
@@ -1,83 +1,197 @@
 import 'package:flutter/material.dart';
+import 'package:forex_currency_conversion/forex_currency_conversion.dart';
 
-class ConverterPage extends StatefulWidget{
+class ConverterPage extends StatefulWidget {
   const ConverterPage({super.key});
 
   @override
   State createState() => _CurrencyConv();
 }
 
-class _CurrencyConv extends State{
-  TextEditingController textEditingControllerfrom = TextEditingController();
-  double value = 0;
+class _CurrencyConv extends State {
+  TextEditingController textEditingController1 = TextEditingController(
+    text: '0.00',
+  );
+  TextEditingController textEditingController2 = TextEditingController(
+    text: '0.00',
+  );
+  String dropDownValue1 = "USD";
+  String dropDownValue2 = "USD";
+  double usdToFirst = 1;
+  double usdToSecond = 1;
+  final fx = Forex();
+  List<String> availableCurrencies = ['USD'];
+  List<DropdownMenuItem> dropdownCurrencies = [
+    DropdownMenuItem(value: 'USD', child: Text('USD')),
+  ];
+
+  void getCurrencies() async {
+    availableCurrencies = await fx.getAvailableCurrencies();
+    availableCurrencies.sort();
+    setState(() {
+      dropdownCurrencies = List.generate(
+        availableCurrencies.length,
+        (int index) => DropdownMenuItem(
+          value: availableCurrencies[index],
+          child: Text(availableCurrencies[index]),
+        ),
+      );
+    });
+  }
+
+  void getPrice(dynamic val, int i) async {
+    if (i == 1) {
+      usdToFirst = await fx.getCurrencyConverted(
+        destinationCurrency: val.toString(),
+        sourceCurrency: "USD",
+        sourceAmount: 1,
+      );
+      textEditingController2 = TextEditingController(
+        text: (double.parse(
+                  textEditingController1.text.isEmpty
+                      ? '0.0'
+                      : textEditingController1.text,
+                ) *
+                usdToSecond /
+                usdToFirst)
+            .toStringAsFixed(2),
+      );
+    }
+    if (i == 2) {
+      usdToSecond = await fx.getCurrencyConverted(
+        destinationCurrency: val.toString(),
+        sourceCurrency: "USD",
+        sourceAmount: 1,
+      );
+      textEditingController1 = TextEditingController(
+        text: (double.parse(
+                  textEditingController2.text.isEmpty
+                      ? '0.0'
+                      : textEditingController2.text,
+                ) *
+                usdToFirst /
+                usdToSecond)
+            .toStringAsFixed(2),
+      );
+    }
+    setState(() {});
+  }
 
   @override
-  Widget build(BuildContext context){
+  void initState() {
+    super.initState();
+    getCurrencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: Text(
-          'Currency Converter App',
-          style: TextStyle(
-            color: Colors.white,
-          ),
+          'Real-Time Currency Conversion',
+          style: TextStyle(color: Colors.white),
         ),
         centerTitle: true,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(12),
-            child: Text(
-              'Select Currency to Convert From:',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+          Text(
+            textAlign: TextAlign.center,
+            '1 $dropDownValue1 = ${(usdToSecond / usdToFirst).toStringAsFixed(2)} $dropDownValue2',
+            style: TextStyle(fontSize: 30),
           ),
-          Padding(
-            padding: const EdgeInsets.all(12),
-            child: TextField(
-              controller: textEditingControllerfrom,
-              keyboardType: TextInputType.numberWithOptions(decimal:true),
-              style: TextStyle(
-                fontSize: 20,
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 20,
+                  children: [
+                    DropdownButton(
+                      itemHeight: 120,
+                      iconSize: 60,
+                      style: TextStyle(fontSize: 50, color: Colors.black),
+                      value: dropDownValue1,
+                      items: dropdownCurrencies,
+                      onChanged: (value1) {
+                        dropDownValue1 = value1;
+                        getPrice(dropDownValue1, 1);
+                      },
+                    ),
+                    Text('  1 USD =\n $usdToFirst $dropDownValue1'),
+                  ],
+                ),
               ),
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.currency_rupee), 
+              Column(
+                spacing: 50,
+                children: [
+                  SizedBox(
+                    width: 275,
+                    child: TextField(
+                      controller: textEditingController1,
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true,
+                      ),
+                      style: TextStyle(fontSize: 20),
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.arrow_forward_ios_sharp),
+                      ),
+                      onChanged: (whatever) {
+                        getPrice(dropDownValue1, 1);
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 275,
+                    child: TextField(
+                      onChanged: (whatever) {
+                        getPrice(dropDownValue2, 2);
+                      },
+                      controller: textEditingController2,
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true,
+                      ),
+                      style: TextStyle(fontSize: 20),
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.arrow_forward_ios_sharp),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12),
-            child: TextButton(
-              onPressed: (){
-                setState(() {
-                  value = double.parse(textEditingControllerfrom.text)*10;
-                });
-              },
-               style: TextButton.styleFrom(
-                backgroundColor: Colors.black,
-                foregroundColor: Colors.white,
-                minimumSize: Size(double.infinity, 20),
-                textStyle: TextStyle(fontSize: 20)
-               ),
-               child: Text(
-                'Convert',
-               ),
-               ),
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    DropdownButton(
+                      itemHeight: 120,
+                      iconSize: 60,
+                      style: TextStyle(fontSize: 50, color: Colors.black),
+                      value: dropDownValue2,
+                      items: dropdownCurrencies,
+                      onChanged: (value2) {
+                        dropDownValue2 = value2;
+                        getPrice(dropDownValue2, 2);
+                      },
+                    ),
+                    Text('  1 USD =\n $usdToSecond $dropDownValue2'),
+                  ],
+                ),
+              ),
+            ],
           ),
           Text(
-            'Result: ${value.toStringAsFixed(2)}',
-            style: TextStyle(
-              fontSize: 25,
-            ),
-          )
+            textAlign: TextAlign.center,
+            '1 $dropDownValue2 = ${(usdToFirst / usdToSecond).toStringAsFixed(2)} $dropDownValue1',
+            style: TextStyle(fontSize: 30),
+          ),
         ],
       ),
-      
     );
   }
 }
